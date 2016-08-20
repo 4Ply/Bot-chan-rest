5a22816570e6665ace816be3eecd712d00cca1da
Pawel Maskowicz <gamer.4ply@gmail.com>
2016-08-20 13:08:22 +0200

    Use jitpack.io for models artifact

--- src/main/java/com/netply/web/security/oauth/GoogleOAuthValidatorImpl.java
+++ src/main/java/com/netply/web/security/oauth/GoogleOAuthValidatorImpl.java
@@ -41,7 +41,7 @@ public class GoogleOAuthValidatorImpl implements GoogleOAuthValidator {
 
             // Get profile information from payload
             String email = payload.getEmail();
-            boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+            boolean emailVerified = payload.getEmailVerified();
             String name = (String) payload.get("name");
             String pictureUrl = (String) payload.get("picture");
             String locale = (String) payload.get("locale");
