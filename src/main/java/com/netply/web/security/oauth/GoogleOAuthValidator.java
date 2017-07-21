d92fae23414ca512c1cdc66cc59298d1424a7eb2
Pawel Maskowicz <gamer.4ply@gmail.com>
2017-07-21 18:14:30 +0200

    Remove auth layer

--- src/main/java/com/netply/web/security/oauth/GoogleOAuthValidator.java
+++
@@ -1,8 +0,0 @@
-package com.netply.web.security.oauth;
-
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-
-public interface GoogleOAuthValidator {
-    boolean validate(String idTokenString) throws GeneralSecurityException, IOException;
-}
