1364412dd2796e1b50a94fd8948359f99a1d5760
Pawel Maskowicz <gamer.4ply@gmail.com>
2017-10-24 20:24:06 +0200

    Stop permissions endpoints from truncating urls containing '.'

--- src/main/java/com/netply/botchan/web/rest/permissions/PermissionController.java
+++ src/main/java/com/netply/botchan/web/rest/permissions/PermissionController.java
@@ -15,26 +15,26 @@ public class PermissionController {
         this.permissionManager = permissionManager;
     }
 
-    @RequestMapping(value = "/hasPermission/{permission}", produces = "application/json", method = RequestMethod.GET)
+    @RequestMapping(value = "/hasPermission/{permission:.+}", produces = "application/json", method = RequestMethod.GET)
     public @ResponseBody
     Boolean hasPermission(@PathVariable(value = "permission") String permission,
                           @RequestParam("platformID") Integer platformID) {
         return permissionManager.hasPermission(platformID, permission);
     }
 
-    @RequestMapping(value = "/permission/{permission}", consumes = "application/json", produces = "application/json", method = RequestMethod.PUT)
+    @RequestMapping(value = "/permission/{permission:.+}", consumes = "application/json", produces = "application/json", method = RequestMethod.PUT)
     public void addPermission(@PathVariable(value = "permission") String permission,
                               @RequestParam("platformID") Integer platformID) {
         permissionManager.addPermission(platformID, permission);
     }
 
-    @RequestMapping(value = "/permission/{permission}", consumes = "application/json", produces = "application/json", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/permission/{permission:.+}", consumes = "application/json", produces = "application/json", method = RequestMethod.DELETE)
     public void removePermission(@PathVariable(value = "permission") String permission,
                                  @RequestParam("platformID") Integer platformID) {
         permissionManager.removePermission(platformID, permission);
     }
 
-    @RequestMapping(value = "/usersForPermission/{permission}", produces = "application/json", method = RequestMethod.GET)
+    @RequestMapping(value = "/usersForPermission/{permission:.+}", produces = "application/json", method = RequestMethod.GET)
     public @ResponseBody
     List<Integer> usersForPermission(@PathVariable(value = "permission") String permission) {
         return permissionManager.getUsersThatHavePermission(permission);
